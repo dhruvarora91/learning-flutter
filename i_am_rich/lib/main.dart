bef1ed88f2edcd01aa6bd888314eef16b7dfa60b
Dhruv Arora <dhruvarora91@gmail.com>
2022-03-13 23:33:49 +0530

    I am Rich - Added README and migrated flutter app to null safety

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('I am Rich'),
@@ -16,6 +23,6 @@ void main() {
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
